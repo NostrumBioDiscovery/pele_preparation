17ff279dfd9e97482a0374084c7706ec1ab554ff
dani <daniel.soler@e-campus.uab.cat>
2019-07-29 15:02:46 +0200

    Include setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pele_preparation',
+      version='1.0',
+      description='Python scripts to prepare PELE input',
+      author='Daniel Soler',
+      author_email='daniel.soler@nostrumbiodiscovery.com',
+      url='https://github.com/NostrumBioDiscovery/pele_preparation',
+      packages=['distutils', 'distutils.command'],
+     )
